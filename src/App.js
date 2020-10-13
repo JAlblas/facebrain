88d3e5ef6b891f24530baf993d42aa45001e0b01
Jasper Alblas <just_jasper@hotmail.com>
2020-10-13 14:54:38 +0200

    minor design changes

--- src/App.js
+++ src/App.js
@@ -8,38 +8,54 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Particles className='particles'
-          params={{
-            particles: {
-              line_linked: {
-                shadow: {
-                  enable: true,
-                  color: "#3CA9D1",
-                  blur: 5
-                }
-              }, 
-              number: {
-                value: 25,
-                density: {
-                  enable: true,
-                  value_area: 500
+  
+    constructor() {
+      super();
+      this.state = {
+        input: ''
+      }
+    }
+
+    onInputChange = (event) => {
+      console.log(event.target.value);
+    }
+
+    onButtonSubmit = () => {
+      console.log('submit');
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <Particles className='particles'
+            params={{
+              particles: {
+                line_linked: {
+                  shadow: {
+                    enable: true,
+                    color: "#3CA9D1",
+                    blur: 5
+                  }
+                }, 
+                number: {
+                  value: 25,
+                  density: {
+                    enable: true,
+                    value_area: 500
+                  }
                 }
               }
-            }
-          }}
-          />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        {/*
-        <FaceRecognition />*/}
-      </div>
-    );
-  }
+            }}
+            />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+          {/*
+          <FaceRecognition />*/}
+        </div>
+      );
+    }
 }
 
 export default App;
